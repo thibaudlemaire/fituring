409cd638070c4f6ca692da35ed03590fe921c5eb
Thibaud Lemaire <thibaud.lemaire@telecom-paristech.fr>
2016-02-05 15:15:02 +0100

    Créations des interfaces pour les modules Kinect, Détection de rythme et Synthèse audio

--- backend/src/interfaces/KinectInterface.java
+++ backend/src/interfaces/KinectInterface.java
@@ -1,5 +1,35 @@
 package interfaces;
 
-public interface KinectInterface {
+import edu.ufl.digitalworlds.j4k.Skeleton;
 
+/**
+ * Kinect module interface
+ * @author thibaud
+ *
+ */
+public interface KinectInterface {
+	
+	/** 
+	 * This function initialize the Kinect Module
+	 */
+	public void initKinectModule();
+	
+	/**
+	 * This function setup a listener for the Kinect
+	 * When the Kinect module receive new interesting datas, listeners are called
+	 * @param l a KinectListener
+	 */
+	public void setListener(KinectListener l);
+	
+	/** 
+	 * This function returns current Skeleton 
+	 * @return currentSkeleton
+	 */
+	public Skeleton getSkeleton();
+	
+	/** This function returns the number of dancers
+	 * @return number of dancers
+	 */
+	public int getNumberOfDancers();
+	
 }
