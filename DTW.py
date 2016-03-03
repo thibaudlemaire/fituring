4f3401530d50d2f0da7cab0fd99107d74da693e7
Alexandre Poinso <alexandre.poinso@gmail.com>
2016-03-03 23:54:14 +0100

    Ajout de la méthode interpolation + Modifications mineures

--- DTW.py
+++ DTW.py
@@ -1,9 +1,9 @@
 from math import *
-from sys import *
+from sys import maxsize
 import numpy as np
 from matplotlib import pyplot as plt
 
-maxINT=sys.maxsize
+maxINT=maxsize
 
 def distance_euclid(a,b):
     return sqrt((b[0]-a[0])**2+(b[1]-a[1])**2)
@@ -35,8 +35,7 @@ Test1 = [[0,3],[1,3],[2,3],[3,3],[2,2],[1,1],[0,0],[1,0],[2,0],[3,0]]    #"Z" dr
 Test2 = [[0,3],[1,3.5],[2,3.5],[3,3],[2,2],[1,1],[0,0],[1,0.5],[2,0.5],[3,0]] #"Z" courbé
 Test3 = [[0,0],[0,1],[0,2],[0,3],[1,2],[2,1],[3,0],[3,1],[3,2],[3,3]]  #"N" droit 
 
-# M=np.asarray(Test1)
-
+#pour pouvoir tracer les graphes, renvoie des arraylists d'abscisses et d'ordonnées
 def prog(chaine):
     resultX=[]
     resultY=[]
@@ -46,6 +45,8 @@ def prog(chaine):
     chaineX=np.asarray(resultX)
     chaineY=np.asarray(resultY)
     return chaineX, chaineY
+
+"""
     
 Test1Abs = prog(Test1)[0]
 Test1Ord = prog(Test1)[1]    
@@ -54,13 +55,36 @@ Test2Ord = prog(Test2)[1]
 Test3Abs = prog(Test3)[0]
 Test3Ord = prog(Test3)[1] 
 
-"""
 
 plt.axis([-1,4,-1,4])
-plt.plot(Test1Abs, Test1Ord, 'r', linewidth=2)
-plt.plot(Test2Abs, Test2Ord, 'b', linewidth=2)
-plt.plot(Test3Abs, Test3Ord, 'g', linewidth=2)
+plt.plot(Test1Abs, Test1Ord, 'r', linewidth=2) #rouge
+plt.plot(Test2Abs, Test2Ord, 'b', linewidth=2) #bleu
+plt.plot(Test3Abs, Test3Ord, 'g', linewidth=2) #vert
+
+plt.show()
 
 """
 
+#interpolation linéaire pour que les 2 chaînes aient des points définis aux mêmes temps
+def interpolation(chaine1, chaine2):
+    a=0
+    t1=chaine2[0][2]
+    t2=chaine2[1][2]
+    newChaine=[]
+    for i in range(len (chaine1)):
+        t0=chaine1[i][2]
+        while (t1 > t0 or t2 < t0):
+            a=a+1
+            t1 , t2= chaine2[a][2] , chaine2[a+1][2]
+        if t1==t0 :
+            newChaine.append([chaine2[a]])
+        elif t2==t0 :
+            newChaine.append([chaine2[a+1]])
+        else :
+           x=chaine2[a][0]+(chaine2[a+1][0]-chaine2[a][0])*(t0-t1)/(t2-t1)
+           y=chaine2[a][1]+(chaine2[a+1][1]-chaine2[a][1])*(t0-t1)/(t2-t1)    
+           newChaine.append([x,y,t0])
+    return newChaine                
+            
+        
         
--- backend/src/classification/DTW.java
+++ backend/src/classification/DTW.java
@@ -76,4 +76,33 @@ public class DTW {
 			return c;
 		}
 	}
+	
+	/* Fonction nécessaire afin de pouvoir comparer 2 chaînes dont les coordonnées sont prises aux mêmes instants
+	 * Réaliséeà l'aide d'une interpolation linéaire */
+	private double[][] interpolation(double[][] chaine1, double[][] chaine2) {
+		double[][] result = new double[chaine1.length][3];
+		int a = 0;
+		double t1 = chaine2[0][2];
+		double t2 = chaine2[1][2];
+		for (int i = 0 ; i < chaine1.length ; i++){
+			double t0 = chaine1[i][2];
+			while (t1 > t0 || t2 < t0){
+				a=a+1;
+				t1 = chaine2[a][2];
+				t2 = chaine2[a+1][2];
+			}
+			if (t1 == t0)
+				result[i]=chaine2[a];
+			else if (t2 == t0)
+				result[i]=chaine2[a+1];
+			else {
+				double x = chaine2[a][0]+(chaine2[a+1][0]-chaine2[a][0])*(t0-t1)/(t2-t1);
+				double y = chaine2[a][1]+(chaine2[a+1][1]-chaine2[a][1])*(t0-t1)/(t2-t1);
+				result[i][0]=x;
+				result[i][1]=y;   
+				result[i][2]=t0; 				
+			}
+		}
+		return result; 
+	}
 }
