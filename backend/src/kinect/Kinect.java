2f7c8d398385ecff7ea5229fe68e6da9a133820b
Thibaud Lemaire <thibaud.lemaire@telecom-paristech.fr>
2016-03-08 01:01:33 +0100

    Affichage de l'altitude de la tête

--- backend/src/kinect/Kinect.java
+++ backend/src/kinect/Kinect.java
@@ -1,6 +1,7 @@
 package kinect;
 
 import java.util.Date;
+
 import edu.ufl.digitalworlds.j4k.J4KSDK;
 import edu.ufl.digitalworlds.j4k.Skeleton;
 
@@ -10,41 +11,37 @@ public class Kinect extends J4KSDK
 	Skeleton currentSkeleton = null;
 	int counter=0;
 	long time=0;
-	KinectRecorder kinectRecorder = new KinectRecorder(new XEDConvertApp());
+	//KinectRecorder kinectRecorder = new KinectRecorder(new XEDConvertApp());
 	
 	@Override
 	public void onSkeletonFrameEvent(boolean[] skeleton_tracked, float[] positions, float[] orientations, byte[] joint_status) {
 		System.out.println("New skeleton !");
-		currentSkeleton = Skeleton.getSkeleton(1, skeleton_tracked, positions, orientations, joint_status, getDeviceType());
-	}
-
-	@Override
-	public void onColorFrameEvent(byte[] color_frame) {
-		System.out.println("A new color frame was received.");
-	}
-
-	@Override
-	public void onDepthFrameEvent(short[] depth_frame, byte[] body_index, float[] xyz, float[] uv) {
-		System.out.println("A new depth frame was received.");
+		currentSkeleton = Skeleton.getSkeleton(0, skeleton_tracked, positions, orientations, joint_status, this);
 		
+		System.out.println(currentSkeleton.get3DJointY(Skeleton.HEAD));
+	}
+	
+	@Override
+	public void onDepthFrameEvent(short[] arg0, byte[] arg1, float[] arg2, float[] arg3) 
+	{
 		if(counter==0)
 			time=new Date().getTime();
 		counter+=1;
 	}
 	
+	@Override
+	public void onColorFrameEvent(byte[] arg0) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
 	public static void main(String[] args)
 	{
 		
-		if(System.getProperty("os.arch").toLowerCase().indexOf("64")<0)
-		{
-			System.out.println("WARNING: You are running a 32bit version of Java.");
-			System.out.println("This may reduce significantly the performance of this application.");
-			System.out.println("It is strongly adviced to exit this program and install a 64bit version of Java.\n");
-		}
-		
-		System.out.println("This program will run for about 20 seconds.");
+		System.out.println("Début");
 		Kinect kinect=new Kinect();
-		kinect.start(J4KSDK.COLOR|J4KSDK.DEPTH|J4KSDK.SKELETON);
+		kinect.start(J4KSDK.DEPTH|J4KSDK.SKELETON);
 		
 		
 		//Sleep for 20 seconds.
@@ -55,9 +52,9 @@ public class Kinect extends J4KSDK
 		System.out.println("FPS: "+kinect.counter*1000.0/(new Date().getTime()-kinect.time));
 	}
 
-	public void startRecording(String fileName)
+	/*public void startRecording(String fileName)
 	{
 		kinectRecorder.startRecording(fileName);
-	}
+	}*/
 	
 }
