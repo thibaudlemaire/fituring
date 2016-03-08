9aa6c8441071ff47b3b18c6d7f4279e1e7ff9970
Thibaud Lemaire <thibaud.lemaire@telecom-paristech.fr>
2016-03-08 01:33:34 +0100

    Enregistrement des mouvements dans un fichier csv

--- backend/src/kinect/Kinect.java
+++ backend/src/kinect/Kinect.java
@@ -1,5 +1,7 @@
 package kinect;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Date;
 
 import edu.ufl.digitalworlds.j4k.J4KSDK;
@@ -9,16 +11,28 @@ public class Kinect extends J4KSDK
 {
 	
 	Skeleton currentSkeleton = null;
+	static PrintWriter pw = null;
+	static String filename = "test.csv";
 	int counter=0;
 	long time=0;
 	//KinectRecorder kinectRecorder = new KinectRecorder(new XEDConvertApp());
 	
 	@Override
 	public void onSkeletonFrameEvent(boolean[] skeleton_tracked, float[] positions, float[] orientations, byte[] joint_status) {
-		System.out.println("New skeleton !");
+		//System.out.println("New skeleton !");
 		currentSkeleton = Skeleton.getSkeleton(0, skeleton_tracked, positions, orientations, joint_status, this);
 		
-		System.out.println(currentSkeleton.get3DJointY(Skeleton.HEAD));
+		pw.print(new Date().getTime()-time);
+		for(int i=0; i<Skeleton.JOINT_COUNT; i++)
+		{
+			pw.print(";");
+			pw.print(currentSkeleton.get3DJointX(i));
+			pw.print(",");
+			pw.print(currentSkeleton.get3DJointY(i));
+			pw.print(",");
+			pw.print(currentSkeleton.get3DJointZ(i));
+		}
+		pw.println();
 	}
 	
 	@Override
@@ -41,20 +55,36 @@ public class Kinect extends J4KSDK
 		
 		System.out.println("Début");
 		Kinect kinect=new Kinect();
+		
+		try {
+			pw = new PrintWriter(filename);
+		} catch (FileNotFoundException e) 
+		{
+			System.err.println("File not found !");
+		} catch(SecurityException e) {
+			System.err.println("Security error !");
+		} catch (Exception e) {
+			System.err.println("Error !");
+			e.printStackTrace(System.err);
+		} 
+		
+		pw.println("Time;SPINE_BASE;SPINE_MID;NECK;HEAD;SHOULDER_LEFT;ELBOW_LEFT;WRIST_LEFT;HAND_LEFT;"
+				+ "SHOULDER_RIGHT;ELBOW_RIGHT;WRIST_RIGHT;HAND_RIGHT;HIP_LEFT;KNEE_LEFT;"
+				+ "ANKLE_LEFT;FOOT_LEFT;HIP_RIGHT;KNEE_RIGHT;ANKLE_RIGHT;FOOT_RIGHT;"
+				+ "SPINE_SHOULDER;HAND_TIP_LEFT;THUMB_LEFT;HAND_TIP_RIGHT;THUMB_RIGHT");
+		
 		kinect.start(J4KSDK.DEPTH|J4KSDK.SKELETON);
 		
 		
 		//Sleep for 20 seconds.
-		try {Thread.sleep(20000);} catch (InterruptedException e) {}
+		try {Thread.sleep(5000);} catch (InterruptedException e) {}
 		
+		if (pw != null) {
+			try { pw.close(); } catch (Exception e) { } ;
+		}
 		
 		kinect.stop();		
 		System.out.println("FPS: "+kinect.counter*1000.0/(new Date().getTime()-kinect.time));
 	}
-
-	/*public void startRecording(String fileName)
-	{
-		kinectRecorder.startRecording(fileName);
-	}*/
 	
 }
