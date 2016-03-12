1891b8b8e689c7dddc2c6240109dc1388f56187f
Thibaud Lemaire <thibaud@dhcpwifi-23-175.enst.fr>
2016-03-12 14:17:35 +0100

    Préparation de la gestion des listeners

--- backend/src/kinect/Kinect.java
+++ backend/src/kinect/Kinect.java
@@ -1,7 +1,5 @@
 package kinect;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.Date;
 
 import javax.swing.event.EventListenerList;
@@ -15,9 +13,6 @@ public class Kinect extends J4KSDK implements KinectInterface
 {
 	
 	Skeleton currentSkeleton = null;
-	static PrintWriter pw = null;
-	static String filename = "datas/clap.csv";
-	static int captureTime = 10000;
 	int counter=0;
 	long time=0;
 	
@@ -28,17 +23,6 @@ public class Kinect extends J4KSDK implements KinectInterface
 		System.out.println("New skeleton !");
 		currentSkeleton = Skeleton.getSkeleton(0, skeleton_tracked, positions, orientations, joint_status, this);
 		
-		pw.print(new Date().getTime()-time);
-		for(int i=0; i<Skeleton.JOINT_COUNT; i++)
-		{
-			pw.print(";");
-			pw.print(currentSkeleton.get3DJointX(i));
-			pw.print(",");
-			pw.print(currentSkeleton.get3DJointY(i));
-			pw.print(",");
-			pw.print(currentSkeleton.get3DJointZ(i));
-		}
-		pw.println();
 	}
 	
 	@Override
@@ -66,8 +50,7 @@ public class Kinect extends J4KSDK implements KinectInterface
 
 	@Override
 	public void setListener(KinectListener l) {
-		// TODO Auto-generated method stub
-		
+		listeners.add(KinectListener.class, l );
 	}
 
 	@Override
@@ -90,56 +73,12 @@ public class Kinect extends J4KSDK implements KinectInterface
 	public static void main(String[] args)
 	{
 		
-		System.out.println("D�but");
+		System.out.println("Debut");
 		Kinect kinect=new Kinect();
-		
-		try {
-			pw = new PrintWriter(filename);
-		} catch (FileNotFoundException e) 
-		{
-			System.err.println("File not found !");
-		} catch(SecurityException e) {
-			System.err.println("Security error !");
-		} catch (Exception e) {
-			System.err.println("Error !");
-			e.printStackTrace(System.err);
-		} 
-
-		pw.println("Time;"
-				+ "SPINE_BASE_X,SPINE_BASE_Y,SPINE_BASE_Z;"
-				+ "SPINE_MID_X,SPINE_MID_Y,SPINE_MID_Z;"
-				+ "NECK_X,NECK_Y,NECK_Z;"
-				+ "HEAD_X,HEAD_Y,HEAD_Z;"
-				+ "SHOULDER_LEFT_X,SHOULDER_LEFT_Y,SHOULDER_LEFT_Z;"
-				+ "ELBOW_LEFT_X,ELBOW_LEFT_Y,ELBOW_LEFT_Z;"
-				+ "WRIST_LEFT_X,WRIST_LEFT_Y,WRIST_LEFT_Z;"
-				+ "HAND_LEFT_X,HAND_LEFT_Y,HAND_LEFT_Z;"
-				+ "SHOULDER_RIGHT_X,SHOULDER_RIGHT_Y,SHOULDER_RIGHT_Z;"
-				+ "ELBOW_RIGHT_X,ELBOW_RIGHT_Y,ELBOW_RIGHT_Z;"
-				+ "WRIST_RIGHT_X,WRIST_RIGHT_Y,WRIST_RIGHT_Z;"
-				+ "HAND_RIGHT_X,HAND_RIGHT_Y,HAND_RIGHT_Z;"
-				+ "HIP_LEFT_X,HIP_LEFT_Y,HIP_LEFT_Z;"
-				+ "KNEE_LEFT_X,KNEE_LEFT_Y,KNEE_LEFT_Z;"
-				+ "ANKLE_LEFT_X,ANKLE_LEFT_Y,ANKLE_LEFT_Z;"
-				+ "FOOT_LEFT_X,FOOT_LEFT_Y,FOOT_LEFT_Z;"
-				+ "HIP_RIGHT_X,HIP_RIGHT_Y,HIP_RIGHT_Z;"
-				+ "KNEE_RIGHT_X,KNEE_RIGHT_Y,KNEE_RIGHT_Z;"
-				+ "ANKLE_RIGHT_X,ANKLE_RIGHT_Y,ANKLE_RIGHT_Z;"
-				+ "FOOT_RIGHT_X,FOOT_RIGHT_Y,FOOT_RIGHT_Z;"
-				+ "SPINE_SHOULDER_X,SPINE_SHOULDER_Y,SPINE_SHOULDER_Z;"
-				+ "HAND_TIP_LEFT_X,HAND_TIP_LEFT_Y,HAND_TIP_LEFT_Z;"
-				+ "THUMB_LEFT_X,THUMB_LEFT_Y,THUMB_LEFT_Z;"
-				+ "HAND_TIP_RIGHT_X,HAND_TIP_RIGHT_Y,HAND_TIP_RIGHT_Z;"
-				+ "THUMB_RIGHT_X,THUMB_RIGHT_Y,THUMB_RIGHT_Z");
 		
 		kinect.start(J4KSDK.DEPTH|J4KSDK.SKELETON);
 		
-		
-		try {Thread.sleep(captureTime);} catch (InterruptedException e) {}
-		
-		if (pw != null) {
-			try { pw.close(); } catch (Exception e) { } ;
-		}
+		try {Thread.sleep(10000);} catch (InterruptedException e) {}
 		
 		kinect.stop();		
 		System.out.println("FPS: "+kinect.counter*1000.0/(new Date().getTime()-kinect.time));
