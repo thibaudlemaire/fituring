033b520e390de4b4753a6e2f31e30b820044674c
Thibaud Lemaire <thibaud@dhcpwifi-23-175.enst.fr>
2016-03-12 18:52:56 +0100

    Suppression des overrides et ajout de commentaires

--- backend/src/kinect/Kinect.java
+++ backend/src/kinect/Kinect.java
@@ -10,17 +10,16 @@ import interfaces.KinectListenerInterface;
 
 public class Kinect extends J4KSDK implements KinectInterface
 {
-	
-	Skeleton currentSkeleton = null;
+	Skeleton currentSkeleton = null; // Last skeleton received
 	int counter=0; // Used to calculate FPS
 	long time=0; // Used to calculate FPS
 	
+	// List of EventListener used to update each data's module
 	private final EventListenerList listeners = new EventListenerList();
 	
 	/**
 	 * Event-called when new skeleton is received
 	 */
-	@Override
 	public void onSkeletonFrameEvent(boolean[] skeleton_tracked, float[] positions, float[] orientations, byte[] joint_status) {
 		System.out.println("New skeleton !");
 		
@@ -38,7 +37,6 @@ public class Kinect extends J4KSDK implements KinectInterface
 	/**
 	 * Event-called when depthFrame are received
 	 */
-	@Override
 	public void onDepthFrameEvent(short[] arg0, byte[] arg1, float[] arg2, float[] arg3) 
 	{
 		// Calculation of the FPS
@@ -50,7 +48,6 @@ public class Kinect extends J4KSDK implements KinectInterface
 	/**
 	 * Event-called when colorFrame are received
 	 */
-	@Override
 	public void onColorFrameEvent(byte[] arg0) 
 	{
 		// NTD
@@ -61,7 +58,6 @@ public class Kinect extends J4KSDK implements KinectInterface
 	/**
 	 * This function init the Kinect Module
 	 */
-	@Override
 	public void initKinectModule() {
 		// Init Kinect, datas, current skeleton, etc...
 		start(J4KSDK.DEPTH|J4KSDK.SKELETON);
@@ -70,7 +66,6 @@ public class Kinect extends J4KSDK implements KinectInterface
 	/**
 	 * This function set a new listener in order to get new skeleton datas
 	 */
-	@Override
 	public void setListener(KinectListenerInterface l) {
 		listeners.add(KinectListenerInterface.class, l );
 	}
@@ -78,7 +73,6 @@ public class Kinect extends J4KSDK implements KinectInterface
 	/** 
 	 * This function returns the current skeleton
 	 */
-	@Override
 	public Skeleton getSkeleton() {
 		return currentSkeleton;
 	}
@@ -87,7 +81,6 @@ public class Kinect extends J4KSDK implements KinectInterface
 	 * This function returns the amount of dancers
 	 * NOT YET IMPLEMENTED
 	 */
-	@Override
 	public int getNumberOfDancers() {
 		return 1;
 	}
@@ -96,12 +89,15 @@ public class Kinect extends J4KSDK implements KinectInterface
 	 * This function return the video 
 	 * NOT YET IMPLEMENTED
 	 */
-	@Override
 	public Object getVideo() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	
+	/** 
+	 * This function returns the global FPS rate
+	 * @return FPS
+	 */
 	public long getFPS()
 	{
 		return (new Date().getTime()-time)/counter;
