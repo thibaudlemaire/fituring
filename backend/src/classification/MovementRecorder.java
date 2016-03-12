84eda6a36f356f97138531081b35fb3716d55906
Thibaud Lemaire <thibaud@dhcpwifi-23-175.enst.fr>
2016-03-12 17:22:36 +0100

    Finalisation de l'enregistreur en vue des tests

--- backend/src/classification/MovementRecorder.java
+++ backend/src/classification/MovementRecorder.java
@@ -4,6 +4,7 @@ import edu.ufl.digitalworlds.j4k.Skeleton;
 import interfaces.KinectEventInterface;
 import interfaces.KinectInterface;
 import interfaces.KinectListenerInterface;
+import kinect.Kinect;
 
 public class MovementRecorder implements KinectListenerInterface {
 	
@@ -28,8 +29,25 @@ public class MovementRecorder implements KinectListenerInterface {
 		mvt.steps.add(step);		
 	}
 	
+	public Move getMovement()
+	{
+		return mvt;
+	}
+	
 	public static void main(String[] args)
 	{
+		System.out.println("Debut de l'enregistrement");
+		MovementSerializer ms = new MovementSerializer();
+		Kinect kinect=new Kinect();
+		MovementRecorder mr = new MovementRecorder(kinect);
 		
+		kinect.initKinectModule();
+
+		try {Thread.sleep(10000);} catch (InterruptedException e) {}
+		
+		kinect.stop();	
+		ms.serialize(mr.getMovement(), "data/m1.mvt");
+		
+		System.out.println("FPS: "+kinect.getFPS());
 	}
 }
