aa3c8610023494a4834a3432d003fd2958f6eeaf
Thibaud Lemaire <thibaud@dhcpwifi-23-175.enst.fr>
2016-03-12 13:51:34 +0100

    Ajout de la classe KinectEvent

--- backend/src/interfaces/KinectListener.java
+++ backend/src/interfaces/KinectListener.java
@@ -14,6 +14,6 @@ public interface KinectListener extends EventListener {
 	 * from the Kinect sensor
 	 * @param e A KinectEvent object
 	 */
-	void skeletonReceived(KinectEvent e);
+	void skeletonReceived(KinectEventInterface e);
 
 }
