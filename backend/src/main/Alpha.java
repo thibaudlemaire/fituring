0737e883a17fd2fc70be8fdb2d14f29bb64c9321
Thibaud Lemaire <thibaud@dhcpwifi-23-175.enst.fr>
2016-03-12 15:20:01 +0100

    Essais avec une première version

--- backend/src/main/Alpha.java
+++ backend/src/main/Alpha.java
@@ -1,9 +1,21 @@
 package main;
 
+import kinect.Kinect;
+
 public class Alpha {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		System.out.println("Debut");
+		Kinect kinect=new Kinect();
+		
+		kinect.initKinectModule();
 
+		try {Thread.sleep(10000);} catch (InterruptedException e) {}
+		
+		kinect.stop();	
+		
+		System.out.println("FPS: "+kinect.getFPS());
 	}
 
 }
