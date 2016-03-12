460be2affb1469b19d8626077532f6a659526470
Alexandre Poinso <alexandre.poinso@gmail.com>
2016-03-12 17:19:56 +0100

    Changement de la taille de la file en vue des tests

--- backend/src/classification/DatasFIFO.java
+++ backend/src/classification/DatasFIFO.java
@@ -5,22 +5,24 @@ public class DatasFIFO {
 	double[][] tab;
 	int len;
 	
+	
+	// the size is 236 cause it matchs the duration of our recorded movements
 	public DatasFIFO() {
-		this.tab = new double[100][3];
+		this.tab = new double[236][3];
 		this.len=0;
 	}
 	
 	public void addData(double[] coordinates) {
-		if (len < 99) {
+		if (len < 235) {
 			tab[len+1]=coordinates;
 			len++;
 		}
 		
 		else {
-			for (int i=0; i<98 ; i++) {
+			for (int i=0; i<235 ; i++) {
 				tab[i] = tab[i+1];
 			}
-			tab[99]=coordinates;
+			tab[235]=coordinates;
 		}
 
 	}
