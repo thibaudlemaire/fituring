7f877f8635d0bd00c13f81b6206b144f0582b9cc
Alexandre Poinso <alexandre.poinso@gmail.com>
2016-03-12 13:42:11 +0100

    Début de l'mplémentation de la classe Classification

--- backend/src/classification/Classification.java
+++ backend/src/classification/Classification.java
@@ -1,22 +1,30 @@
 package classification;
 
+import edu.ufl.digitalworlds.j4k.Skeleton;
 import interfaces.ClassificationInterface;
 import interfaces.KinectEvent;
 import interfaces.KinectInterface;
 import interfaces.KinectListener;
 
 public class Classification implements ClassificationInterface, KinectListener {
+	
+	Object BDD ;
 
 	@Override
 	public void initClassificationModule(Object BDD, KinectInterface kinectModule) {
 		// TODO Auto-generated method stub
 		kinectModule.setListener(this);
-		
+		this.BDD = BDD ;	
 	}
+	
+	
 
 	@Override
 	public void skeletonReceived(KinectEvent e) {
 		// TODO Auto-generated method stub
+		Skeleton newSkeleton = e.getNewSkeleton();
+		double[] wristLeftCoordinates = newSkeleton.get3DJoint(Skeleton.WRIST_LEFT);
+		double[] wristRightCoordinates = newSkeleton.get3DJoint(Skeleton.WRIST_RIGHT);
 		
 		
 	}
