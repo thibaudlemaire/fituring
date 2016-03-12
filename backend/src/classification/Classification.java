36d2f872488255182c83532107db8c07afdf2010
Robin SHIN <robin.shin@telecom-paristech.fr>
2016-03-12 16:42:09 +0100

    Création de la structure de donnée des mouvements

--- backend/src/classification/Classification.java
+++ backend/src/classification/Classification.java
@@ -1,5 +1,14 @@
 package classification;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 import edu.ufl.digitalworlds.j4k.Skeleton;
 import interfaces.ClassificationInterface;
 import interfaces.KinectEvent;
@@ -27,6 +36,32 @@ public class Classification implements ClassificationInterface, KinectListener {
 			
 	}
 
-	
+	private static double[][][] getDatasTable(String path) throws FileNotFoundException {
+		BufferedReader reader = null;
+		String line;
+		try {
+			reader = new BufferedReader(new FileReader(path));
+			
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out.println("Fichier introuvable");
+		}
+		while((line = reader.readLine()) != null)
+		{
+			String[] items = line.split(";");
+			long time = Long.parseLong(items[0]);
+			for(int i = 1; i<25; i++)
+			{
+				
+			}
+			
+		}
+			return datasTable;
+	}
 
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		getDatasTable("datas/armsUp.csv");
+	}
 }
