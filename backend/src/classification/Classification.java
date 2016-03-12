8cd5ee414a11a7b3a29ddab7b48117dedc518bd5
Robin SHIN <robin.shin@telecom-paristech.fr>
2016-03-12 17:53:03 +0100

    Retrait d'une méthode inutile

--- backend/src/classification/Classification.java
+++ backend/src/classification/Classification.java
@@ -1,21 +1,13 @@
 package classification;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.regex.Pattern;
-
 import edu.ufl.digitalworlds.j4k.Skeleton;
 import interfaces.ClassificationInterface;
 import interfaces.KinectEvent;
+import interfaces.KinectEventInterface;
 import interfaces.KinectInterface;
-import interfaces.KinectListener;
+import interfaces.KinectListenerInterface;
 
-public class Classification implements ClassificationInterface, KinectListener {
+public class Classification implements ClassificationInterface, KinectListenerInterface {
 	
 	Object BDD ;
 
@@ -27,7 +19,6 @@ public class Classification implements ClassificationInterface, KinectListener {
 	}
 		
 
-	@Override
 	public void skeletonReceived(KinectEvent e) {
 		// TODO Auto-generated method stub
 		Skeleton newSkeleton = e.getNewSkeleton();
@@ -39,32 +30,11 @@ public class Classification implements ClassificationInterface, KinectListener {
 		datasFIFORight.addData(handRightCoordinates);
 	}
 
-	private static double[][][] getDatasTable(String path) throws FileNotFoundException {
-		BufferedReader reader = null;
-		String line;
-		try {
-			reader = new BufferedReader(new FileReader(path));
-			
-		}
-		catch (FileNotFoundException e)
-		{
-			System.out.println("Fichier introuvable");
-		}
-		while((line = reader.readLine()) != null)
-		{
-			String[] items = line.split(";");
-			long time = Long.parseLong(items[0]);
-			for(int i = 1; i<25; i++)
-			{
-				
-			}
-			
-		}
-			return datasTable;
-	}
-
 	
-	public static void main(String[] args) throws FileNotFoundException {
-		getDatasTable("datas/armsUp.csv");
+
+	@Override
+	public void skeletonReceived(KinectEventInterface e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
