374eb73a4af2b2f4a195fac42c70307d4e28cb4c
Thibaud Lemaire <thibaud@200.wifi.rezel.net>
2016-03-12 23:24:33 +0100

    Ajout de l'enregistreur de mvt

--- backend/src/classification/MovementRecorder.java
+++ backend/src/classification/MovementRecorder.java
@@ -4,6 +4,7 @@ import edu.ufl.digitalworlds.j4k.Skeleton;
 import interfaces.KinectEventInterface;
 import interfaces.KinectInterface;
 import interfaces.KinectListenerInterface;
+import kinect.Kinect;
 
 public class MovementRecorder implements KinectListenerInterface {
 	
@@ -32,4 +33,20 @@ public class MovementRecorder implements KinectListenerInterface {
 		return mvt;
 	}
 
+	public static void main(String[] args)
+	{
+		System.out.println("Debut de l'enregistrement");
+		MovementSerializer ms = new MovementSerializer();
+		Kinect kinect  =new Kinect();
+		MovementRecorder mr = new MovementRecorder(kinect);
+		
+		kinect.initKinectModule();
+
+		try {Thread.sleep(10000);} catch (InterruptedException e) {}
+		
+		kinect.stop();	
+		
+		ms.serialize(mr.getMovement(), "datas/m1.mvt");
+	}
 }
+
