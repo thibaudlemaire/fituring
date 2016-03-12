94f61fffd6d56735c48e9f34583bd1d6278290c7
Robin SHIN <robin.shin@telecom-paristech.fr>
2016-03-12 16:42:09 +0100

    Création de la structure de donnée des mouvements

---
+++ backend/src/classification/Move.java
@@ -0,0 +1,19 @@
+package classification;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Move implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -169925548721174155L;
+
+	public ArrayList<Step> steps;
+
+	public Move()
+	{
+	}
+
+}
