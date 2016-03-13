12645839ecf83dd6a681a9d649e81e66fb70fd2a
Thibaud Lemaire <thibaud@200.wifi.rezel.net>
2016-03-13 01:10:06 +0100

    Début de la commande au clavier

--- backend/src/main/Alpha.java
+++ backend/src/main/Alpha.java
@@ -7,13 +7,16 @@ public class Alpha {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Debut");
+		System.out.println("Création des instances");
 		Kinect kinect=new Kinect();
 		Classification cl = new Classification();
-		cl.initClassificationModule(new Object(), kinect);
 		
+		System.out.println("Initialisation des modules");
+		cl.initClassificationModule(new Object(), kinect);
 		kinect.initKinectModule();
 
+		System.out.print("Appuyer sur une Entrer pour démarer l'analyse");
+		
 		try {Thread.sleep(20000);} catch (InterruptedException e) {}
 		
 		kinect.stop();	
