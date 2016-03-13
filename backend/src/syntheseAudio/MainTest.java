6faa93f006084fe9b1cf6ea6a2aa52e4c77a3207
Thibaud Lemaire <thibaud@dhcpwifi-23-175.enst.fr>
2016-03-13 17:17:15 +0100

    Ménage et gestion multithread des sons

--- backend/src/syntheseAudio/MainTest.java
+++ backend/src/syntheseAudio/MainTest.java
@@ -1,28 +1,23 @@
 package syntheseAudio;
 
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
+import java.io.FileNotFoundException;
 
 public class MainTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		/*SourceDataLine sourceLine = new SourceDataLine();
-		try {
-            sourceLine = (SourceDataLine) AudioSystem.getLine(info);
-            sourceLine.open(audioFormat);
-        } catch (LineUnavailableException e) {
-            e.printStackTrace();
-        } */
-		PulsThread pulsThread = new PulsThread(80, "C:/Users/NotAfraid/Documents/Sons-PACT/clap-hall-01.wav");
-		PulsThread puls2Thread = new PulsThread(80, "C:/Users/NotAfraid/Documents/Sons-PACT/clap-hall-01.wav");
+		PulsThread pulsThread = null;
+		AudioThread audioThread = null;
+		try 
+		{
+			pulsThread = new PulsThread("sounds/kick.wav", 400);
+			audioThread = new AudioThread("sounds/clap.wav");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		audioThread.start();
+		pulsThread.start();
 
-		//AudioThread audioThread = new AudioThread("C:/Users/NotAfraid/Documents/Sons-PACT/clap-hall-01.wav");
-		pulsThread.run();
-		puls2Thread.run();
-		//audioThread.run();
 	}
 
 }
