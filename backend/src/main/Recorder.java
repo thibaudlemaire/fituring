7e1bfd8fa49742c311cb3d56577a5ec74c93d13e
Thibaud Lemaire <thibaud.lemaire@telecom-paristech.fr>
2016-03-13 14:31:02 +0100

    Modification rapide

--- backend/src/main/Recorder.java
+++ backend/src/main/Recorder.java
@@ -25,7 +25,6 @@ public class Recorder {
 		{
 			System.out.println("Appuyer sur une Entrer pour démarer l'enregistrement");
 		    strIn = sc.nextLine();
-		    strIn = sc.nextLine();
 		    if(strIn.length() >= 1 && strIn.charAt(0) == 'q')
 		    	break;
 		    
@@ -37,6 +36,7 @@ public class Recorder {
 		    System.out.println("Nommer cet enregistrement");
 		    strIn = sc.nextLine();
 		    ms.serialize(mr.getMovement(), "datas/" + strIn);
+		    mr = new MovementRecorder(kinect);
 		}
 		
 		System.out.println("Stopping Kinect");
