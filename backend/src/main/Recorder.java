1a1228a0379134af2ef1f8d2fe5541d47a7168ad
Thibaud Lemaire <thibaud@MacBook-Pro-de-Thibaud-2.local>
2016-03-13 02:55:17 +0100

    Ajout du controle au clavier

--- backend/src/main/Recorder.java
+++ backend/src/main/Recorder.java
@@ -2,7 +2,8 @@ package main;
 
 import java.util.Scanner;
 
-import classification.Classification;
+import classification.MovementRecorder;
+import classification.MovementSerializer;
 import kinect.Kinect;
 
 public class Recorder {
@@ -10,21 +11,31 @@ public class Recorder {
 	public static void main(String[] args) 
 	{
 	    Scanner sc = new Scanner(System.in);
-
+	    String strIn = "";
+	    
 		System.out.println("Création des instances");
 		Kinect kinect = new Kinect();
-		Classification cl = new Classification();
+		MovementRecorder mr = new MovementRecorder(kinect);
+		MovementSerializer ms = new MovementSerializer();
 		
 		System.out.println("Initialisation des modules");
-		cl.initClassificationModule(new Object(), kinect);
 		kinect.initKinectModule();
 
-		System.out.print("Appuyer sur une Entrer pour démarer l'enregistrement");
-	    sc.nextLine();
-	    
-	    cl.startListening();
-	    
-	    sc.nextLine();
+		while(true)
+		{
+			System.out.println("Appuyer sur une Entrer pour démarer l'enregistrement");
+		    sc.nextLine();
+		    if(strIn == "q")
+		    	break;
+		    mr.startListener();
+		    
+		    sc.nextLine();
+		    mr.stopListener();
+		    
+		    System.out.println("\r\n" + "Nommer cet enregistrement");
+		    strIn = sc.nextLine();
+		    ms.serialize(mr.getMovement(), "datas/" + strIn);
+		}
 		kinect.stop();	
 		
 		sc.close();
