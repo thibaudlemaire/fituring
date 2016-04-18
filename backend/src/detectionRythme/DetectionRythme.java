11574bac98fdda2218ef0dd603885dc7f3af1f59
khadija EL ATTAR SOFI <khadija_elattar@hotmail.com>
2016-04-18 17:09:17 +0200

    imports

--- backend/src/detectionRythme/DetectionRythme.java
+++ backend/src/detectionRythme/DetectionRythme.java
@@ -1,6 +1,10 @@
 package detectionRythme;
 
+import edu.ufl.digitalworlds.j4k.Skeleton;
+import interfaces.KinectEventInterface;
+import interfaces.KinectInterface;
 import interfaces.KinectListenerInterface;
+import interfaces.LectureAudioSimpleInterface;
 import interfaces.RyhtmeInterface;
 
 public class DetectionRythme implements RyhtmeInterface, KinectListenerInterface{
