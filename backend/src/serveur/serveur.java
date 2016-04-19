22271f71e2776657b075502ce40464c62afa9c85
Camille Demasson <camille.demasson@gmail.com>
2016-04-19 14:19:54 +0200

    création de la classe serveur

---
+++ backend/src/serveur/serveur.java
@@ -0,0 +1,19 @@
+package serveur;
+
+import interfaces.ServeurInterface;
+
+public class serveur implements ServeurInterface {
+
+	@Override
+	public int getVolume() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getMusicalStyle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
