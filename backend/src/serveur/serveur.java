058e0d1d5636d7cfa857b65efa19e10515e8db00
Camille Demasson <camille.demasson@gmail.com>
2016-04-25 13:47:09 +0200

    Implémentation du serveur avec erreurs

--- backend/src/serveur/serveur.java
+++ backend/src/serveur/serveur.java
@@ -1,13 +1,39 @@
 package serveur;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import interfaces.ServeurInterface;
 
 public class serveur implements ServeurInterface {
 
 	@Override
 	public int getVolume() {
-		// TODO Auto-generated method stub
-		return 0;
+		int portNumber = Integer.parseInt(args[0]);
+
+		try ( 
+		    ServerSocket serverSocket = new ServerSocket(portNumber);
+		    Socket clientSocket = serverSocket.accept();
+		    PrintWriter out =
+		        new PrintWriter(clientSocket.getOutputStream(), true);
+		    BufferedReader in = new BufferedReader(
+		        new InputStreamReader(clientSocket.getInputStream()));
+		){
+		    String inputLine, outputLine;
+		    // Initiate conversation with client
+		    KnockKnockProtocol kkp = new KnockKnockProtocol();
+		    outputLine = kkp.processInput(null);
+		    out.println(outputLine);
+
+		    while ((inputLine = in.readLine()) != null) {
+		        outputLine = kkp.processInput(inputLine);
+		        out.println(outputLine);
+		        if (outputLine.equals("Bye."))
+		            break;
+		    }
 	}
 
 	@Override
