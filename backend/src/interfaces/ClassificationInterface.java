f54c7fe3fae395dddf4d3ac8a03c76b49584a577
Thibaud Lemaire <thibaud@dhcpwifi-22-139.enst.fr>
2016-05-01 18:34:29 +0200

    Modification des interfaces en vue de la beta

--- backend/src/interfaces/ClassificationInterface.java
+++ backend/src/interfaces/ClassificationInterface.java
@@ -5,8 +5,14 @@ public interface ClassificationInterface {
 	/**
 	 * This function initialize the Classification Module
 	 */
-	public void initClassificationModule(Object BDD, KinectInterface kinectModule, LectureInterface audio);
+	public void initClassificationModule(KinectInterface kinectModule,  MovementFoundInterface engineModule);
 	
+	/**
+	 * Function used to add a gesture tu recognize
+	 * @param path to the gesture
+	 * @return ID of the gesture
+	 */
+	public int addGesture(String path);
 	/**
 	 * This function starts the listening of kinect skeleton
 	 */
@@ -15,8 +21,12 @@ public interface ClassificationInterface {
 	/**
 	 * This function stops the listening of kinect skeleton
 	 */
-	public void stopListeninf();
+	public void stopListening();
 	
-	
+	/**
+	 * This function returns the number of gestures analyzed
+	 * @return
+	 */
+	public int getNumberOfGestures();
 	
 }
