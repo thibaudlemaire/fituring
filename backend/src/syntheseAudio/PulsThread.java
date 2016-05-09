bfe7ced2a744b57cb1d1842afc6363c7e2893b62
Thibaud Lemaire <thibaud@MacBook-Pro-de-Thibaud-2.local>
2016-05-09 04:40:02 +0200

    Modification du pulse pour creer un metronome

--- backend/src/syntheseAudio/PulsThread.java
+++ backend/src/syntheseAudio/PulsThread.java
@@ -1,43 +1,27 @@
 package syntheseAudio;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-
-import sun.audio.AudioPlayer;
-import sun.audio.AudioStream;
-
 public class PulsThread extends Thread{
 	
-	private String filePath;
 	private int period; //period : a BPM 
 	private boolean keepPlayin;
 		
-	public PulsThread(String filePath, int period) throws FileNotFoundException
+	public PulsThread(int period)
 	{
 		super();
-		this.filePath = filePath;
 		this.period = period;
-		keepPlayin=true;
+		keepPlayin = true;
 	}
 	
 	public void run()
 	{
 		while(keepPlayin)
 		{
-			try 
-			{
-				{
-					InputStream in = new FileInputStream(filePath);
-				    AudioStream as = new AudioStream(in);
-				    AudioPlayer.player.start(as);
-					Thread.sleep(60000/period); // sleep takes an argument in milliseconds
-				}
-			}
-			catch (Exception e) 
-			{
+			try {
+				Thread.sleep(60000/period);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
+			} // sleep takes an argument in milliseconds
 		}
 	}
 	
@@ -46,6 +30,6 @@ public class PulsThread extends Thread{
 	}
 	
 	public void stopPulsThread (){
-		keepPlayin=false;
+		keepPlayin = false;
 	}
 }
