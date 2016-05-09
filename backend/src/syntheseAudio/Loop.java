76f33bf6d53c472ce605057596ab0297a87acd9a
Thibaud Lemaire <thibaud@MacBook-Pro-de-Thibaud-2.local>
2016-05-09 05:07:08 +0200

    Fin gestion Listener/Métronome

--- backend/src/syntheseAudio/Loop.java
+++ backend/src/syntheseAudio/Loop.java
@@ -1,7 +1,27 @@
 package syntheseAudio;
 
+import java.io.FileNotFoundException;
 import java.util.EventListener;
 
 public class Loop implements EventListener {
+	
+	private String path;
+	
+	public Loop(String path)
+	{
+		this.path = path;
+	}
+	
+	public void beat()
+	{
+		AudioThread audioThread = null;
+		try 
+		{
+			audioThread = new AudioThread(path);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		audioThread.start();
+	}
 
 }
